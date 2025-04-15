e86c7e5b2f315d21f76328ef20b5bbccb660781d
LUKZConnectz <163946633+LUKZConnectz@users.noreply.github.com>
2025-04-16 00:25:58 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const prizes = [
+  { label: "รางวัลใหญ่", type: "credit", amount: 1000 },
+  { label: "รางวัลเล็ก", type: "credit", amount: 100 },
+  { label: "เกลือ", type: "none", amount: 0 },
+  { label: "หมุนฟรี 3 รอบ", type: "free_spin", amount: 3 },
+  { label: "หมุนอีกรอบ", type: "free_spin", amount: 1 },
+];
